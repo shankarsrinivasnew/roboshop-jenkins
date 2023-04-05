6c68ca1dd9a57b27854e08c8df5e4d59fd95d1cb
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-05 23:01:13 +0530

    dfg

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "jenkins_job" "example" {
   depends_on = [jenkins_folder.foldersr]
   count      = length(var.jobs)
   name       = lookup(element(var.jobs, count.index), "name", null)
-  folder     = lookup(element(var.jobs, count.index), "folder", null)
+  folder     = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
   template = templatefile("${path.module}/sn-job.xml", {
     repo_url    = lookup(element(var.jobs, count.index), "repo_url", null)
   })
