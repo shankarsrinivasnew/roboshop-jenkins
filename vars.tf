8a0947e84cfe970f1075b2743ad5400c80402576
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-06 11:19:40 +0530

    sdf

--- vars.tf
+++ vars.tf
@@ -1,10 +1,19 @@
 variable "folders" {
-  default = ["infra"]
+  default = ["infra", "CI-pipeline"]
 }
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "infra", repo_url = "https://github.com/shankarsrinivasnew/roboshop-infra" }
+    { name = "roboshop", folder = "infra", repo_url = "https://github.com/shankarsrinivasnew/roboshop-infra" },
+    { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/cart" },
+    { name = "catalogue", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/catalogue" },
+    { name = "user", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/user" },
+    { name = "payment", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/payment" },
+    { name = "shipping", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/shipping" },
+    { name = "dispatch", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/dispatch" },
+    { name = "frontend", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/frontend" }
+
   ]
 
 }
+
