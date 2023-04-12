87c4244a659f1761a7b749cf6ca81e5ecb47c5ea
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-12 11:56:12 +0530

    asdf

--- main.tf
+++ main.tf
@@ -1,25 +1,53 @@
 
-resource "jenkins_folder" "foldersr" {
-  count = length(var.folders)
-  name  = element(var.folders, count.index)
+// single branch pipelines
+
+resource "jenkins_folder" "s-foldersr" {
+  count = length(var.s-folders)
+  name  = element(var.s-folders, count.index)
 }
 
-resource "jenkins_job" "example" {
-  depends_on = [jenkins_folder.foldersr]
+resource "jenkins_job" "s-jobr" {
+  depends_on = [jenkins_folder.s-foldersr]
 
-  count = length(var.jobs)
+  count = length(var.s-jobs)
 
-  name   = lookup(element(var.jobs, count.index), "name", null)
-  folder = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
+  name   = lookup(element(var.s-jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.s-jobs, count.index), "folder", null)}"
 
   template = templatefile("${path.module}/sn-job.xml", {
-    repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
-    name     = lookup(element(var.jobs, count.index), "name", null)
+    repo_url = lookup(element(var.s-jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.s-jobs, count.index), "name", null)
   })
   /* lifecycle {
     ignore_changes = [template]
   } */
 }
+
+// Multi branch pipelines
+
+resource "jenkins_folder" "m-foldersr" {
+  count = length(var.m-folders)
+  name  = element(var.m-folders, count.index)
+}
+
+resource "jenkins_job" "m-jobr" {
+  depends_on = [jenkins_folder.m-foldersr]
+
+  count = length(var.m-jobs)
+
+  name   = lookup(element(var.m-jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.m-jobs, count.index), "folder", null)}"
+
+  template = templatefile("${path.module}/mb-job.xml", {
+    repo_url = lookup(element(var.m-jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.m-jobs, count.index), "name", null)
+  })
+  /* lifecycle {
+    ignore_changes = [template]
+  } */
+}
+
+// Jenkins R53 Record
 
 resource "aws_route53_record" "jenkinsr" {
   zone_id = "Z0607165JC9NKEPWSMH2"
--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
-variable "folders" {
+variable "s-folders" {
   default = ["infra", "CI-pipeline"]
 }
 
-variable "jobs" {
+variable "s-jobs" {
   default = [
     { name = "roboshop", folder = "infra", repo_url = "https://github.com/shankarsrinivasnew/roboshop-infra" },
     { name = "cart", folder = "CI-pipeline", repo_url = "https://github.com/shankarsrinivasnew/cart" },
@@ -17,3 +17,21 @@ variable "jobs" {
 
 }
 
+variable "m-folders" {
+  default = ["CI-pipeline-multi"]
+}
+
+
+variable "m-jobs" {
+  default = [
+    { name = "cart", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/cart" },
+    { name = "catalogue", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/catalogue" },
+    { name = "user", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/user" },
+    { name = "payment", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/payment" },
+    { name = "shipping", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/shipping" },
+    { name = "dispatch", folder = "CI-pipelin-multi", repo_url = "https://github.com/shankarsrinivasnew/dispatch" },
+    { name = "frontend", folder = "CI-pipeline-multi", repo_url = "https://github.com/shankarsrinivasnew/frontend" }
+
+  ]
+
+}
